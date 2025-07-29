d357962dddea991c511096a4f5f10b9dad2b34ae
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2025-07-29 19:04:02 -0300

    criei objeto

--- script.js
+++ script.js
@@ -9,4 +9,14 @@ let linguagens = ["JavaScript", "Python", "C++"];
 console.log(linguagens[0]);
 
 const item = document.querySelector("#lista");
-item.textContent = linguagens[0];
+item.textContent = `${linguagens[0]}, ${linguagens[1]} e ${linguagens[2]}`;
+
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ªsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+
+}
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.anoLetivo;
